880edf6033f0187f84acc27fd70f2f4d6043c898
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-03-27 12:54:01 +0600

    js10-regexp-1.js problem solve

---
+++ js10-regexp-1.js
@@ -0,0 +1,15 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    var re = /^(a|e|i|o|u).*\1$/;
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+var test = regexVar();
+console.log(test(test('bcd')));
